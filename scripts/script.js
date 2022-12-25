d581af9e47e9c9df664ec85c9c246d9b6f1aea2f
rashidali001 <rashidestonian@gmail.com>
2022-12-26 00:36:13 +0300

    Ecom

---
+++ scripts/script.js
@@ -0,0 +1,88 @@
+/* Opening Overlay nav */
+
+function openNav()
+{
+    let body = document.querySelector('body');
+    let overlay = document.querySelector('#overlay');
+
+    overlay.style.display = 'block';
+    body.style.backgroundColor = 'hsl(0, 0%, 70%)';
+}
+
+/* Closing Overlay nav */
+
+function closeNav()
+{
+    let overlay = document.querySelector('#overlay');
+    let body = document.querySelector('body');
+    
+    overlay.style.display = 'none';
+    body.style.backgroundColor = 'hsl(0, 0%, 100%)';
+
+}
+
+let images =  ['/images/image-product-1.jpg', './images/image-product-2.jpg', './images/image-product-3.jpg', './images/image-product-4.jpg'];
+
+/* Courasel */
+let img =document.querySelector('#courasel-img');
+
+img.src = images[0];
+let change = 0;
+function courasel(value){
+    change = change + value;
+
+    if (change < 0){
+        change = images.length-1;
+        img.src = images[change];
+        return;
+    }
+
+    if (change > (images.length-1))
+    {
+        change = 0;
+        img.src = images[change];
+        return;
+    }
+    img.src = images[change];  
+}
+
+/** Closing cart */
+
+function closeCart(){
+    let cart = document.querySelector('div.cart');
+   
+
+    cart.style.display = 'none';
+    
+}
+
+function openCart(){
+    let cart = document.querySelector('div.cart');
+    let body = document.querySelector('body');
+
+    cart.style.display = "block";
+    body.style.backgroundColor = 'hsl(0, 0%, 90%)';
+}
+
+/* Count */
+let num = 0
+let number = document.querySelector('#number');
+number.value = num;
+function count(value){
+    num = parseInt(number.value);
+    num += value;
+    
+    if (num < 0)
+    {
+        num = 0;
+    }
+    
+    number.value = num;
+    
+
+
+}
+
+
+
+
